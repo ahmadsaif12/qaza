9cc43b9703b8789bc42a3222ba0eacba75493dc8
Moid Khan <mk07216@st.habib.edu.pk>
2026-05-22 17:18:34 +0500

    redeploy

--- src/auth.ts
+++ src/auth.ts
@@ -45,7 +45,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     async session({ session, token }) {
       if (token && session.user) {
-        session.user.id = token.id as string
+        session.user.id = (token.id || token.sub) as string
       }
       return session
     }
